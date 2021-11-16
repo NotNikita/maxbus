2b706e638a7f62f2cba5b2ebc51c906e9cf692c4
NotNikita <nikitabsuir@gmail.com>
2021-11-17 00:55:08 +0300

    styles are working

--- js/bookCall.js
+++ js/bookCall.js
@@ -12,37 +12,7 @@ closePopupBtn.addEventListener('click', (e) => {
     popUp.classList.remove('active');
 })
 
-// Sending and validating
-
-
-const sendDataForm = document.getElementById('sendDataForm');
-
-{ /* 1. put this in form, 
-     2. change style selector for input[type=submit] -> button
-     3. delete input submit
-     <button id="sendDataPopUp"
-    onclick="return sendData(this.form, this.form.client_name, this.form.phone_number);">Отправить
-</button> */}
-function sendData(form, client_name, phone_number) {
-    const validationResult = validateDataFromForm(client_name.value, phone_number.value);
-    const res = validationResult.every(test => test === true);
-
-    if (res) {
-        Email.send({
-            Host: "smtp.mail.ru",
-            Username: "max.bus@list.ru",
-            Password: "PBLZy3H7kAGerxhYYH7t",
-            To: 'max.bus@list.ru',
-            From: "max.bus@list.ru",
-            Subject: `Запрос ${client_name.value} на звонок`,
-            Body: `Клиент ${client_name.value} запросил звонок на номер ${phone_number.value}. \n 
-            Запрос поступил ${getCurrData()}`,
-        }).then(
-            message => alert("Запрос успешно отправлен")
-        );
-        popUp.classList.remove('active');
-    }
-}
+// Sending and validating scripts
 
 function validateDataFromForm(strName, strPhone) {
     let regExpName = /[A-Za-zА-Яа-яЁё]{3,}/;
@@ -62,7 +32,6 @@ function getCurrData() {
 submitFormFromInput = function () {
     let xname = document.forms["0"]["name"].value;
     let xphone = document.forms["0"]["phone"].value;
-    // second validation LOL
     const res = validateDataFromForm(xname, xphone).every(test => test === true);
 
     if (res) {
@@ -79,24 +48,5 @@ submitFormFromInput = function () {
             message => alert("Запрос успешно отправлен")
         );
         popUp.classList.remove('active');
-    }
-}
-
-
-// <form  onsubmit="validateForm()"/>
-function validateForm() {
-    let xname = document.forms["0"]["name"].value;
-    let xphone = document.forms["0"]["phone"].value;
-
-    const res = validateDataFromForm(xname, xphone).every(test => test === true);
-
-    if (res) {
-        console.log('Everything is OK');
-        return true;
-    } else {
-        alert('BAD INPUT');
-        console.log(`xname: ${xname}`);
-        console.log(`xphone: ${xphone}`);
-        return false;
     }
 }
